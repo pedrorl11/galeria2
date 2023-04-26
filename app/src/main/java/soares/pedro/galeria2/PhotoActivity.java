89eb0362eea4bfaa9d00bf6c376b1648c469226e
20211tiimi0125 <pedro05042006@gmail.com>
2023-04-26 12:57:52 -0300

    Aula 26 - passo 6

--- app/src/main/java/soares/pedro/galeria2/PhotoActivity.java
+++ app/src/main/java/soares/pedro/galeria2/PhotoActivity.java
@@ -1,19 +1,22 @@
 package soares.pedro.galeria2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo);
 
-        Toolbar toolbar = findViewById(R.id.tbMain);
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
